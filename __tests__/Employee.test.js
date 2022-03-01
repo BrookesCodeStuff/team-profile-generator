d6532320a578cdf43147aeb1de71c176ac15ce00
Brooke Paglia <brookepaglia@gmail.com>
2022-02-28 21:54:03 -0500

    add employee object and tests

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,35 @@
+const Employee = require('../lib/Employee');
+
+test('creates an employee', () => {
+  const employee = new Employee('Jane');
+
+  expect(employee.name).toEqual(expect.any(String));
+});
+
+test("gets employee's name", () => {
+  const employee = new Employee('Jane');
+
+  expect(employee.getName()).toBe('Jane');
+});
+
+test("gets an employee's id", () => {
+  const employee = new Employee('Jane');
+
+  employee.id = 1;
+
+  expect(employee.getId()).toBeGreaterThanOrEqual(1);
+});
+
+test("gets an empoyee's email address", () => {
+  const employee = new Employee('Jane');
+
+  employee.email = 'jane@company.com';
+
+  expect(employee.getEmail()).toEqual(expect.stringContaining('@'));
+});
+
+test("gets an employee's role", () => {
+  const employee = new Employee('Jane');
+
+  expect(employee.role).toBe('Employee');
+});
